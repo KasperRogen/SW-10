2b4dc2ba371567dd2ced81d8ae7c289e1e01c3a1
Jens Burla Johansen <jens@burla.dk>
2020-02-11 14:20:13 +0100

    Separated algorithm from Unity

---
+++ Project_Satellites/Assets/Algorithm/INode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+interface INode
+{
+    int ID { get; set; }
+    List<INode> ReachableNodes { get; set; }
+    Position TargetPosition { get; set; }
+
+    void Communicate(Constants.Commands command);
+    void Communicate(Constants.Commands command, ConstellationPlan plan);
+}
