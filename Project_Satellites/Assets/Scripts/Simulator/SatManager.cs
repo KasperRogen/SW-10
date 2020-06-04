0103ebc7a04bba407b5a1dc780002069f696987a
kasper <kasperhangard@hotmail.com>
2020-06-04 03:01:24 +0200

    Packed, Locked & Loaded

---
+++ Project_Satellites/Assets/Scripts/Simulator/SatManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SatManager : MonoBehaviour
+{
+    public static SatManager _instance;
+
+    public List<SatelliteComms> satellites = new List<SatelliteComms>();
+    public List<MessageProps> SentMessages = new List<MessageProps>();
+    public int SatIndex = 0;
+
+    private void Start()
+    {
+        _instance = this;
+
+
+    }
+
+    public class MessageProps
+    {
+        public MessageProps(Vector3 startvect, Vector3 endvect, Color color, float Duration)
+        {
+            this.StartVect = startvect;
+            this.EndVect = endvect;
+            this.Color = color;
+            this.Duration = Duration;
+        }
+
+        public Vector3 StartVect, EndVect;
+        public Color Color;
+        public float Duration;
+    }
+
+}
---
+++ Project_Satellites/Assets/Scripts/Simulator/SimulationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationManager : MonoBehaviour
+{
+
+    public static SimulationManager _instance;
+
+    public bool SimMinutesToSeconds;
+
+    private void Awake()
+    {
+#if UNITY_EDITOR
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 150;
+#endif
+    }
+    private void Start()
+    {
+        _instance = this;
+    }
+
+}
